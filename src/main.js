a72297d477dc2475a29a0b356c9ef764ad387fc0
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-09 21:54:11 +0300

    `if (video.seeking) return` optimization
    The earlier implementation would start seeking to a new frame, even
    though the previous frame hadn't been painted to the screen. Adding
    `if (video.seeking) return` prevents frames from being skipped.

--- src/main.js
+++ src/main.js
@@ -8,6 +8,8 @@ const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
 
 /** Scrub the video playback to the appropriate time depending on the scroll position. */
 const scrub = () => {
+  // Don't interrupt in-progress video seeking.
+  if (video.seeking) return
   const offsetPx = video.getBoundingClientRect().top
   const o = offsetPx
   const w = window.innerHeight
