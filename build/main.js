712277387047936c17987be0d1e1f9467839a1c5
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-09 22:21:22 +0300

    Added `build` folder for hosting on GitHub Pages

---
+++ build/main.js
@@ -0,0 +1,20 @@
+import './_snowpack/pkg/modern-normalize.css.proxy.js'
+import './styles.css.proxy.js'
+
+/** @type {HTMLVideoElement} */
+const video = document.querySelector('#scroll-vid')
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max)
+
+/** Scrub the video playback to the appropriate time depending on the scroll position. */
+const scrub = () => {
+  // Don't interrupt in-progress video seeking.
+  if (video.seeking) return
+  const offsetPx = video.getBoundingClientRect().top
+  const o = offsetPx
+  const w = window.innerHeight
+  const scrubProgress = clamp((w - o) / (2 * w), 0, 1)
+  video.currentTime = video.duration * scrubProgress
+}
+
+window.addEventListener('scroll', scrub)
---
+++ build/styles.css.proxy.js
@@ -0,0 +1,10 @@
+// [snowpack] add styles to the page (skip if no document exists)
+if (typeof document !== 'undefined') {
+  const code = "body {\n  background-color: #e6e5e6;\n  color: #333;\n}\n\n.filler {\n  height: 100vh;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n}\n\n#scroll-vid {\n  height: 100vh;\n  width: 100%;\n  object-fit: cover;\n}";
+
+  const styleEl = document.createElement("style");
+  const codeEl = document.createTextNode(code);
+  styleEl.type = 'text/css';
+  styleEl.appendChild(codeEl);
+  document.head.appendChild(styleEl);
+}
