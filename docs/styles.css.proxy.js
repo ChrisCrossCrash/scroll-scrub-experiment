d0261d44a7a7bbb7ce683b8a7f7328b1592f733c
Christopher Kumm <ChrisLKumm@protonmail.com>
2022-02-10 10:38:15 +0300

    Rebuilt for GitHub Pages

--- docs/styles.css.proxy.js
+++ docs/styles.css.proxy.js
@@ -1,6 +1,6 @@
 // [snowpack] add styles to the page (skip if no document exists)
 if (typeof document !== 'undefined') {
-  const code = "body {\n  background-color: #e6e5e6;\n  color: #333;\n}\n\n.filler {\n  height: 100vh;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n}\n\n#scroll-vid {\n  height: 100vh;\n  width: 100%;\n  object-fit: cover;\n}";
+  const code = "body {\n  background-color: #fff;\n  color: #14181b;\n}\n\n.filler {\n  height: 100vh;\n  display: flex;\n  align-items: center;\n  justify-content: center;\n}\n\n#scroll-vid {\n  height: 100vh;\n  width: 100%;\n  object-fit: cover;\n}";
 
   const styleEl = document.createElement("style");
   const codeEl = document.createTextNode(code);
